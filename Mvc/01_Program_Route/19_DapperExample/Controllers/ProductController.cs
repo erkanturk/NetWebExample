32db16275362f01d3a59503c25caf4be44774e15
erkanturk <refres98@gmail.com>
2025-02-08 10:48:32 +0300

    dapper

--- Mvc/01_Program_Route/19_DapperExample/Controllers/ProductController.cs
+++ Mvc/01_Program_Route/19_DapperExample/Controllers/ProductController.cs
@@ -5,20 +5,23 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace _19_DapperExample.Controllers
 {
-    public class ProductController:Controller
+    public class ProductController : Controller
     {
         private readonly DapperContext _context;
+
         public ProductController(DapperContext context)
         {
             _context = context;
         }
+
+        // Tüm ürünleri ve kategorileri listeleyen ana sayfa methodu
         public async Task<IActionResult> Index()
         {
-
+            // ürünleri ve kategorileri birleştirerek seçen sql sorgusu
             var query = "select * from Products inner join Categories on Products.CategoryId = Categories.CategoryId";
             using (var connection = _context.CreateConnetion())
             {
-
+                // Dapper ile çoklu tablo sorgulama
                 var products = await connection.QueryAsync<Product, Category, Product>(
                     query,
                     (product, category) =>
@@ -33,65 +36,99 @@ namespace _19_DapperExample.Controllers
 
             }
 
+
         }
+
+        // Ürün oluşturacağımız sayfa
         public IActionResult Create()
         {
             return View();
         }
+
+
         [HttpPost]
         public async Task<IActionResult> Create(Product product)
         {
-            var query = "Insert into Products (Name,Price,CategoryId) Values (@name,@price,@categoryId)";
-            using (var connetion = _context.CreateConnetion())
+            var query = "INSERT INTO Products (Name,Price,CategoryId) VALUES (@Name,@Price,@CategoryId)";
+
+            using (var connection = _context.CreateConnetion())
             {
-                await connetion.ExecuteAsync(query, product);
+                await connection.ExecuteAsync(query, product);
+                // işlem tamamlandığında anasayfaya yönlendir.
                 return RedirectToAction("Index");
             }
+
             return View(product);
         }
+
+        // Ürün düzenleme sayfasını döndüren method
+
         public async Task<IActionResult> Edit(int id)
         {
-            var query = "Select * from Products Where ProductId=@Id";
+            // Gelen id li ürünün sorgusu
+            var query = "SELECT * FROM Products WHERE ProductId = @Id";
+
             using (var connection = _context.CreateConnetion())
             {
                 var product = await connection.QuerySingleOrDefaultAsync<Product>(query, new { Id = id });
+
                 if (product == null)
                 {
                     return NotFound();
                 }
+
                 return View(product);
             }
+
         }
+
+
         [HttpPost]
         public async Task<IActionResult> Edit(Product product)
         {
-            var query = "Update Products Set Name=@name,Price=@price,CategoryId=@categoryId where ProductId=@productId";
+            // Gelen ürüne göre güncelleme sorgusu
+            var query = "UPDATE Products SET Name = @Name, Price = @Price, CategoryId = @CategoryId WHERE ProductId = @ProductId";
+
             using (var connection = _context.CreateConnetion())
             {
                 await connection.ExecuteAsync(query, product);
+
                 return RedirectToAction("Index");
             }
+
         }
+
+        // Ürünü silme sayfasını döndüren method
         public async Task<IActionResult> Delete(int id)
         {
-            var query = "Select * from Products where ProductId=@id";
+            // Gelen id li ürünün sorgusu
+            var query = "SELECT * FROM Products WHERE ProductId = @Id";
+
             using (var connection = _context.CreateConnetion())
             {
                 var product = await connection.QuerySingleOrDefaultAsync<Product>(query, new { Id = id });
+
                 if (product == null)
                 {
                     return NotFound("Product Not Found");
                 }
+
                 return View(product);
             }
+
         }
+
+
         [HttpPost, ActionName("Delete")]
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
-            var query = "Delete from Product where ProductId=@Id";
+            // Gelen id li ürünü silme sorgusu
+            var query = "DELETE FROM Products WHERE ProductId = @Id";
+
             using (var connection = _context.CreateConnetion())
             {
                 var result = await connection.ExecuteAsync(query, new { Id = id });
+
                 if (result > 0)
                 {
                     ViewBag.Message = "Product Delete Successfully";
@@ -100,21 +137,31 @@ namespace _19_DapperExample.Controllers
                 {
                     ViewBag.Message = "Product Delete Failed";
                 }
-                return View("DeleteResult");
+                return View("DeleteResult"); // Silme sonucunu göstereceğim sayfa
             }
+
         }
+
+        // Ürünün detay sayfasını döndüren method
         public async Task<IActionResult> Details(int id)
         {
-            var query = "Select * from Products where ProductId=@Id";
+            // Gelen id li ürünün sorgusu
+            var query = "SELECT * FROM Products WHERE ProductId = @Id";
+
             using (var connection = _context.CreateConnetion())
             {
                 var product = await connection.QuerySingleOrDefaultAsync<Product>(query, new { Id = id });
+
                 if (product == null)
                 {
                     return NotFound("Product Not Found");
                 }
+
                 return View(product);
             }
+
         }
+
+
     }
 }
